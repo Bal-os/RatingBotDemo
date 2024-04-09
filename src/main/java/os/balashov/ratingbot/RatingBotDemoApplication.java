03c19876ad6c2cf53f73904cc6f377089fa2eee2
Bal-os <sbal.stels@GMAIL.COM>
2024-04-09 16:11:26 +0300

    Create: create empty spring boot service

---
+++ src/main/java/os/balashov/ratingbot/RatingBotDemoApplication.java
@@ -0,0 +1,13 @@
+package os.balashov.ratingbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatingBotDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RatingBotDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/os/balashov/ratingbot/RatingBotDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package os.balashov.ratingbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingBotDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
